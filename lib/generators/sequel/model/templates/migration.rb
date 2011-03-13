0d8f6fcd97ce01c96ec7e269f92ea3537adecb0b
PoTa <pota@mosfet.hu>
2011-03-13 03:08:30 +0100

    Adding migration generation when generating models.

---
+++ lib/generators/sequel/model/templates/migration.rb
@@ -0,0 +1,14 @@
+class <%= migration_file_name.camelize %>Migration < Sequel::Migration
+  def up
+    create_table :<%= table_name %> do
+      primary_key :id
+<% attributes.each do |attribute| -%>
+      <%= attribute.type_class %> :<%= attribute.name %>
+<% end -%>
+    end
+  end
+
+  def down
+    drop_table :<%= table_name %>
+  end
+end
